2674dce792393b607a97ee993a5fdc3d5774f23c
Shane Bryzak <sbryzak@gmail.com>
2011-08-15 08:11:04 +1000

    fix dependencies

--- velocity/impl/src/main/java/org/jboss/seam/mail/templating/velocity/CDIVelocityContext.java
+++ velocity/impl/src/main/java/org/jboss/seam/mail/templating/velocity/CDIVelocityContext.java
@@ -24,7 +24,7 @@ import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 
 import org.apache.velocity.context.AbstractContext;
-import org.jboss.logging.Logger;
+import org.jboss.seam.solder.logging.Logger;
 
 /**
  * @author Cody Lerum

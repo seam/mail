8369836b0888f79f10fddf4f40e0546a30b52971
Cody Lerum <cody.lerum@gmail.com>
2011-08-16 17:25:59 -0600

    update tests and pom for Solder 3.1.0.Beta1

--- examples/sendmail/src/test/java/org/jboss/seam/mail/VelocityMailMessageTest.java
+++ examples/sendmail/src/test/java/org/jboss/seam/mail/VelocityMailMessageTest.java
@@ -75,8 +75,8 @@ public class VelocityMailMessageTest {
                 .addPackages(true, VelocityMailMessageTest.class.getPackage())
                 // workaround for Weld EE embedded not properly reading Seam Solder jar
                 .addAsLibrary(
-                        ShrinkWrap.create(ZipImporter.class, "seam-solder-3.0.0.Final.jar")
-                                .importFrom(MavenArtifactResolver.resolve("org.jboss.seam.solder:seam-solder:3.0.0.Final"))
+                        ShrinkWrap.create(ZipImporter.class, "seam-solder-3.1.0.Beta1.jar")
+                                .importFrom(MavenArtifactResolver.resolve("org.jboss.seam.solder:seam-solder:3.1.0.Beta1"))
                                 .as(JavaArchive.class))
                 .addAsLibraries(MavenArtifactResolver.resolve("org.subethamail:subethasmtp:3.1.4"),
                         MavenArtifactResolver.resolve("org.apache.velocity:velocity:1.6.4"),

4573897a5f602b253d4e70f827fa0ce8442b94a7
LightGuard <lightguard.jp@gmail.com>
2011-10-03 15:25:22 -0600

    Bringing Mail Module into structural compliance
    Removing the old structure
    
    Modifying pom to reflect structure changes
    
    Modifying other poms to reflect changes
    
    Maven compiles the project now.
    
    Adding package-info.java with @Requires

---
+++ impl/src/main/java/org/jboss/seam/mail/templating/render/package-info.java
@@ -0,0 +1,4 @@
+@Requires("org.jboss.seam.mail.templating.TemplateProvider")
+package org.jboss.seam.mail.templating.render;
+
+import org.jboss.seam.solder.core.Requires;
---
+++ impl/src/main/java/org/jboss/seam/mail/templating/velocity/package-info.java
@@ -0,0 +1,4 @@
+@Requires("org.apache.velocity.app.VelocityEngine")
+package org.jboss.seam.mail.templating.velocity;
+
+import org.jboss.seam.solder.core.Requires;
